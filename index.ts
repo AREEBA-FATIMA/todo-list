5028cf4d804df09528b3432c79d14b6d014fac26
AREEBA-FATIMA <areebafatima2457@gmail.com>
2024-04-05 17:15:10 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+let todos = [] 
+let condition = true
+
+while(condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            message: "What do you want to add to Todos?",
+            type: "input",
+        },
+        {
+            name: "addMore",
+            message: "Do you want to add more?",
+            type: "confirm",
+            default: false 
+        }
+    ]);
+
+    todos.push(addTask.todo); 
+    condition = addTask.addMore; 
+}
+
+console.log(todos); 
